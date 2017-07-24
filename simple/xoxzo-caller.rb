5e1b8acaa2da86f372afc2c4f8a9e8db6863ed5a
Akira Nonaka <nonaka@mac.com>
2017-07-24 16:44:00 +0900

    WIP add pattern match

--- simple/xoxzo-caller.rb
+++ simple/xoxzo-caller.rb
@@ -12,6 +12,13 @@ rc = JSON.parse(response.body)
 
 url = rc['url']
 
+
+def process_message(text)
+  if text =~ /^call (\d+) *(.*)/
+    p "call %s msg=<%s>" % [$1, $2]
+  end
+end
+
 EM.run do
   # Web Socketインスタンスの立ち上げ
   ws = Faye::WebSocket::Client.new(url)
@@ -23,12 +30,16 @@ EM.run do
 
   # RTM APIから情報を受け取った時の処理
   ws.on :message do |event|
+    data = JSON.parse(event.data)
+    if data["type"] == "message"
+      process_message(data['text'])
+    end
     p [:message, JSON.parse(event.data)]
   end
 
   # 接続が切断した時の処理
   ws.on :close do
-    p [:close, event.code]
+    p [:close]
     ws = nil
     EM.stop
   end
