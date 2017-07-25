bad83c78782f74adcd591e4cb691c5408fdc574d
Akira Nonaka <nonaka@mac.com>
2017-07-25 17:08:34 +0900

    WIP fix variable scope

--- simple/xoxzo-caller.rb
+++ simple/xoxzo-caller.rb
@@ -11,36 +11,14 @@ response = HTTP.post("https://slack.com/api/rtm.start", params: {
 })
 
 rc = JSON.parse(response.body)
-
 url = rc['url']
-
-sid = ENV['XOXZO_API_SID']
-token = ENV['XOXZO_API_AUTH_TOKEN']
-$xc = XoxzoClient.new(sid,token)
-
-def process_message(text)
-  if text =~ /^call *(\d+) *(.*)/
-    p 'call %s msg=<%s>' % [$1, $2]
-    caller = '05012345678'
-    recipient = $1
-    msg = $2
-    recipient='+81'+ recipient.sub(/^0/,'') # remove if first char is 0
-    res = $xc.call_tts_playback(caller: caller, recipient: recipient, tts_message: msg, tts_lang:"ja")
-    if res.errors != nil
-      p res
-      exit -1
-    end
-    ws.send({
-                type: 'message',
-                text: "こんにちは <@#{data['user']}> さん. #{recipient}に電話します。",
-                channel: data['channel']
-            }.to_json)
-  end
-end
 
 EM.run do
   # Web Socketインスタンスの立ち上げ
   ws = Faye::WebSocket::Client.new(url)
+  sid = ENV['XOXZO_API_SID']
+  token = ENV['XOXZO_API_AUTH_TOKEN']
+  xc = XoxzoClient.new(sid,token)
 
   #  接続が確立した時の処理
   ws.on :open do
@@ -51,7 +29,24 @@ EM.run do
   ws.on :message do |event|
     data = JSON.parse(event.data)
     if data['type'] == 'message'
-      process_message(data['text'])
+      text = data['text']
+      if text =~ /^call *(\d+) *(.*)/
+        p 'call %s msg=<%s>' % [$1, $2]
+        caller = '05012345678'
+        recipient = $1
+        msg = $2
+        recipient='+81'+ recipient.sub(/^0/,'') # remove if first char is 0
+        ws.send({
+                    type: 'message',
+                    text: "こんにちは <@#{data['user']}> さん. #{recipient}に電話します。",
+                    channel: data['channel']
+                }.to_json)
+        res = xc.call_tts_playback(caller: caller, recipient: recipient, tts_message: msg, tts_lang:"ja")
+        if res.errors != nil
+          p res
+          exit -1
+        end
+      end
     end
     p [:message, JSON.parse(event.data)]
   end
